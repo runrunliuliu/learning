70dcdea1b58cdb353284bbee4767e1f0e7b0bfe7
liuliu <liuliu.tju@gmail.com>
2012-11-08 20:13:04 +0800

    mod
    add mod function to show currying

--- src/currying/currying.scala
+++ src/currying/currying.scala
@@ -8,14 +8,29 @@ package currying
  * To change this template use File | Settings | File Templates.
  */
 
-object exercise extends Application {
+object exercise{
 
+  /**
+   * exercise one
+   */
   def product(f: Int => Int)(a: Int, b: Int): Int =
     if (a > b) 1
     else f(a) * product(f)(a + 1, b)
 
+  /**
+   * exercise two
+   *
+   */
+  def modN(n: Int)(x: Int) = ((x % n) == 0)
+  val mod5 = modN(5) _
+  val l = (1 to 30).toList
 
-  println(product(x => x * x)(3, 4))
 
+  def main(args: Array[String]) {
 
+    println(product(x => x * x)(3, 4))
+
+    println(l filter mod5)
+
+  }
 }
